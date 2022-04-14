54ed85da413ddc19e8e85c39e7f55a39873936e8
SongJin <31265254+AIMWLI@users.noreply.github.com>
2022-04-14 12:00:46 +0800

    add echo route and cache integration

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
 from app.config import Config
+from app.cache import TTLCache
 
 app = Flask(__name__)
 cfg = Config()
+cache = TTLCache(ttl=30)
 
 
 @app.route("/")
@@ -12,7 +14,13 @@ def hello():
 
 @app.route("/health")
 def health():
-    return jsonify({"status": "ok", "host": cfg.HOST})
+    return jsonify({"status": "ok", "host": cfg.HOST, "port": cfg.PORT})
+
+
+@app.route("/echo", methods=["POST"])
+def echo():
+    data = request.get_json(force=True)
+    return jsonify({"echo": data})
 
 
 if __name__ == "__main__":
