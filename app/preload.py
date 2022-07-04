877fe6ff1dbcb43fb32e1b9f5ab5ec0c77746736
SongJin <31265254+AIMWLI@users.noreply.github.com>
2022-07-04 16:08:27 +0800

    add cache preload util

---
+++ app/preload.py
@@ -0,0 +1,12 @@
+import json
+from pathlib import Path
+
+
+def preload_cache(cache, data_path):
+    path = Path(data_path)
+    if not path.exists():
+        return
+    with open(path) as f:
+        data = json.load(f)
+    for key, value in data.items():
+        cache.set(key, value)
