dcd9f0375456ce2c9fe252830d5594d9e97b9983
SongJin <31265254+AIMWLI@users.noreply.github.com>
2022-08-22 12:36:56 +0800

    update main with config and health endpoint

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from app.config import Config
+
 app = Flask(__name__)
+cfg = Config()
+
 
 @app.route("/")
 def hello():
     return "Hello from Python!"
 
+
+@app.route("/health")
+def health():
+    return jsonify({"status": "ok", "host": cfg.HOST})
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host=cfg.HOST, port=cfg.PORT)
