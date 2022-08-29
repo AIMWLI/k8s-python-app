f9bbb396fc8dea2dd1ab1c53f8d7f44b2afb3b7a
SongJin <31265254+AIMWLI@users.noreply.github.com>
2022-08-29 12:04:32 +0800

    test unique helper

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,5 @@
 import pytest
-from app.utils import chunks
+from app.utils import chunks, unique_preserve_order
 
 
 def test_chunks():
@@ -8,3 +8,8 @@ def test_chunks():
 
 def test_chunks_odd():
     assert list(chunks([1, 2, 3], 2)) == [[1, 2], [3]]
+
+
+def test_unique_preserve_order():
+    assert unique_preserve_order([1, 2, 1, 3, 2]) == [1, 2, 3]
+    assert unique_preserve_order([]) == []
