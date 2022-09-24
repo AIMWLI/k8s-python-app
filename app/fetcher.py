ff49ccd6c0a6222e60aeaba111e1c12d4d65dd59
SongJin <31265254+AIMWLI@users.noreply.github.com>
2022-09-24 18:22:01 +0800

    add retry to fetcher

--- app/fetcher.py
+++ app/fetcher.py
@@ -3,13 +3,19 @@ import httpx
 
 
 class AsyncFetcher:
-    def __init__(self, base_url, timeout=10):
+    def __init__(self, base_url, timeout=10, max_retries=2):
         self.client = httpx.AsyncClient(base_url=base_url, timeout=timeout)
+        self.max_retries = max_retries
 
     async def get(self, path):
-        r = await self.client.get(path)
-        r.raise_for_status()
-        return r.json()
+        for _ in range(self.max_retries):
+            try:
+                r = await self.client.get(path)
+                r.raise_for_status()
+                return r.json()
+            except (httpx.TimeoutException, httpx.HTTPStatusError):
+                continue
+        return None
 
     async def close(self):
         await self.client.aclose()
